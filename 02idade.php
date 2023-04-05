03842fff4530bbacbd7a67d10b05bf6077c68acc
gurian-1974 <100541003+gurian-1974@users.noreply.github.com>
2023-04-05 08:46:53 -0300

    atualização estudo php

--- 02idade.php
+++ 02idade.php
@@ -41,10 +41,21 @@
 
  $idade =  date("Y") - $ano;
 
-    echo "$nome e $sexo e tem  $idade anos de idade";
+    echo "<br>$nome e $sexo e tem  $idade anos de idade";
+    if($idade >= 18 && $idade < 65){
+        echo "<br> $nome vc deve votar";
+    }elseif($idade < 16){
+        echo "<br> $nome vc nao pode votar";
+    }
+
+    elseif($idade >= 16 && $idade < 18){
+        echo "<br>$nome seu voto e facultativo";
+    }else{
+        echo "<br>$nome seu voto e facutativo";
+    }
 
     ?>
-
+    <br>
     <a href="./formulario01.html">retorna</a>
 </body>
 
